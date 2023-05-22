b7dba689d11bb7b175ced7a02ddfc37ac4371270
JudyAlashqar <judyalashqar123@gmail.com>
2023-05-22 21:50:31 +0300

    second

--- src/main/java/SortList.java
+++ src/main/java/SortList.java
@@ -1,16 +1,45 @@
 public abstract class SortList {
 
     public Entry head;
+    public int succeededDeletions = 0;
+    public int failedDeletions = 0;
+    public int succeededContain = 0;
+    public int failedContain = 0;
 
     public SortList() {
         this.head = new Entry(Integer.MIN_VALUE);
-       this.head.next =new Entry(Integer.MAX_VALUE);
+        this.head.next =new Entry(Integer.MAX_VALUE);
     }
 
     public  abstract  boolean add(Integer obj);
     public  abstract  boolean remove(Integer obj);
     public  abstract  boolean contain(Integer obj);
 
+    public int count()
+    {
+        int length = 0;
+        Entry curr = this.head;
+        while (curr != null){
+            length += 1;
+            curr = curr.next;
+        }
+        return length;
+    }
+
+    public boolean checkSorted()
+    {
+        boolean check = true;
+        Entry curr = this.head;
+        while (curr.next != null){
+            if (curr.object > curr.next.object)
+            {
+                check = false;
+                break;
+            }
+            curr = curr.next;
+        }
+        return check;
+    }
     public void printList(){
         Entry curr = this.head;
         while (curr != null){
